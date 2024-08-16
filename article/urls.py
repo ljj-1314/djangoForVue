a2f5004c7c7af2204460e438caff0978421fcff9
lanjunjie <1951721920@qq.com>
2024-08-16 11:40:08 +0800

    增加文章模块

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,8 @@ import article.views as apiViews
 
 urlpatterns = [
     path('createArticle', apiViews.create),
+    path('updateArticle', apiViews.update),
     path('getPagedList', apiViews.getPagedList),
     path('getDetail', apiViews.getDetail),
+    path('deleteArticle', apiViews.delete),
 ]
--- article/views.py
+++ article/views.py
@@ -8,6 +8,7 @@ from django.db.models import F
 from django.db.models.functions import Concat
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils import timezone
 
 from article.models import UserArticle
 
@@ -45,6 +46,54 @@ def create(request):
         article.save()
 
         return JsonResponse({'message': '文章创建成功', 'data': {'no': article.no}, 'code': '200'}, status=200)
+    except json.JSONDecodeError as e:
+        return JsonResponse({'message': '请求数据格式错误', 'code': '400'}, status=400)
+    except IntegrityError as e:
+        print('错误1', e)
+        return JsonResponse({'message': str(e), 'code': '400'}, status=400)
+    except Exception as e:
+        print('错误2', e)
+        return JsonResponse({'message': str(e), 'code': '500'}, status=500)
+
+
+def update(request):
+    if request.method != 'POST':
+        return JsonResponse({'message': '无效的请求方法', 'code': '405'}, status=405)
+
+    try:
+        data = json.loads(request.body)
+        no = data.get('no')
+        title = data.get('title')
+        content = data.get('content')
+        user_id = data.get('user_id')
+
+        if not title:
+            return JsonResponse({'message': '标题不能为空', 'code': '400'}, status=400)
+        if not user_id:
+            return JsonResponse({'message': '用户ID不能为空', 'code': '400'}, status=400)
+        try:
+            user = User.objects.get(id=user_id)
+        except ObjectDoesNotExist:
+            return JsonResponse({'message': '用户不存在', 'code': '400'}, status=400)
+
+        if not user.is_active:
+            return JsonResponse({'message': '用户未激活', 'code': '400'}, status=400)
+        try:
+            article = UserArticle.objects.get(no=no)
+        except ObjectDoesNotExist:
+            return JsonResponse({'message': '不存在该文章，请重试', 'code': '400'}, status=400)
+
+            # 创建并保存文章
+        article_updated = UserArticle.objects.filter(no=no, author=user).update(
+            title=title,
+            content=content,
+            updateTime=timezone.now()  # 如果需要更新更新时间
+        )
+
+        if article_updated == 1:
+            return JsonResponse({'message': '文章编辑成功', 'data': {'no': no}, 'code': '200'}, status=200)
+        else:
+            return JsonResponse({'message': '文章更新失败', 'code': '400'}, status=400)
     except json.JSONDecodeError as e:
         return JsonResponse({'message': '请求数据格式错误', 'code': '400'}, status=400)
     except IntegrityError as e:
@@ -134,3 +183,26 @@ def getDetail(request):
         }, status=200)
     else:
         return JsonResponse({'message': '该文章不存在', 'code': '405'}, status=405)
+
+
+def delete(request):
+    if request.method != 'DELETE':
+        return JsonResponse({'message': '无效的请求方法', 'code': '405'}, status=405)
+    no = request.GET.get('no')
+    if not no:
+        return JsonResponse({'message': '序号不能为空', 'code': '400'}, status=400)
+    try:
+        article = UserArticle.objects.filter(no=no).first()
+        if article:
+            article.delete()
+            return JsonResponse({
+                'message': '删除成功',
+                'data': {
+                    'no': no,
+                },
+                'code': '200'
+            }, status=200)
+        else:
+            return JsonResponse({'message': '该文章不存在', 'code': '404'}, status=404)
+    except Exception as e:
+        return JsonResponse({'message': '删除失败: {}'.format(str(e)), 'code': '500'}, status=500)
